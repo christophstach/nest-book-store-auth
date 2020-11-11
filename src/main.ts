10ed176866e11d19b033977c9309ed3de0006ced
Christoph Stach <christoph.stach@gmail.com>
2020-11-11 15:07:38 +0800

    Deploy first test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  const port = process.env.PORT || 3333;
+
+  app.setGlobalPrefix(globalPrefix);
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Book Store Auth API')
+    .setDescription(
+      'Use these endpoints to authenticate with wishlist and receive an access token.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
